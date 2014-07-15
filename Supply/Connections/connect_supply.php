1f9e00efd63309fe2be4f653cdef258824949bf1
Raf Smits <raf_smits@hotmail.com>
2014-07-15 09:31:22 +0200

    Update connect_supply.php

--- Supply/Connections/connect_supply.php
+++ Supply/Connections/connect_supply.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_connect_supply = "supplycvba.be.mysql";
-$database_connect_supply = "supplycvba_be";
-$username_connect_supply = "supplycvba_be";
-$password_connect_supply = "Su12Cv34";
+$hostname_connect_supply = "Enter hostname here";
+$database_connect_supply = "Enter database here";
+$username_connect_supply = "Enter user name here";
+$password_connect_supply = "Enter password here";
 $connect_supply = mysql_connect($hostname_connect_supply, $username_connect_supply, $password_connect_supply) or trigger_error(mysql_error(),E_USER_ERROR);
 ?>
